4ea9ae0418dd56a9c0aea31cae42e865b3561e7d
sanu7045 <sunnytai94020@gmail.com>
2026-04-07 12:11:34 +0530

    fixed the position of images to render properly

--- src/Authenticated/Library/Library.jsx
+++ src/Authenticated/Library/Library.jsx
@@ -7,14 +7,14 @@ function Library({ toggleSidebar, isSidebarOpen }) {
     return (
         <div className={`transition-all duration-300 ${isSidebarOpen ? 'pl-70' : 'pl-0'}`}>
             <LibHeader toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-            <div className="p-8 max-w-7xl mx-auto">
-                <h2 className="text-2xl font-semibold mb-6">Gallery</h2>
+            <div className="p-8 max-w-7xl -mt-5">
+                <h2 className={`text-2xl font-semibold mb-4 ${isSidebarOpen ? 'pl-0' : 'pl-20'}`}>Gallery</h2>
                 {images.length === 0 ? (
-                    <p className="text-gray-500">No images uploaded yet.</p>
+                    <p className="text-gray-500 flex justify-center items-center h-screen">No images uploaded yet.</p>
                 ) : (
-                    <div className="h-xl w-2xl grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    <div className={`w-full scroll-smooth max-h-screen pb-[50vh] overflow-y-auto no-scrollbar grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  ${isSidebarOpen ? 'pl-0' : 'pl-20'}`}>
                         {images.map((img, index) => (
-                            <div key={index} className="aspect-square rounded-xl overflow-hidden border border-gray-200 shadow-sm hover:shadow-md transition-all">
+                            <div key={index} className="aspect-square rounded-3xl overflow-hidden border border-gray-200 shadow-sm hover:shadow-md transition-all">
                                 <img src={img} alt={`Uploaded ${index}`} className="w-full h-full object-cover" />
                             </div>
                         ))}
